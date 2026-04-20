c33552dbc4f885f11456d5d9cc9bfb9921f02ec5
lac <hgiang16260@gmail.com>
2026-04-20 17:51:23 +0700

    tu tu chua xong

--- js/giohang.js
+++ js/giohang.js
@@ -1,29 +1,58 @@
 let list = [
-    { Tensach: "ten", soluong: 1, gia: 1000 },
-    { Tensach: "ten2", soluong: 2, gia: 12000 }
+    { Tensach: "Nà ná na na", soluong: 1, gia: 1000 },
+    { Tensach: "A Chị Mèo", soluong: 2, gia: 12000 }
 ];
 
+let ctlist = [
+       {anh: "../img/adomixi.jpg", tacgia: "toptop", date: "30/2/2026", tomtat: "Nhạc ấn độ cực hay", ten: "Nà ná na na"}
+]
+
 let tbody = document.getElementById("list");
-console.log(window.alert("hihi"))
 function render() {
     tbody.innerHTML = "";
 
     for (let i = 0; i < list.length; i++) {
         tbody.innerHTML += `
-            <tr>
-                     <td>${i + 1}</td>
-                      <td>${list[i].Tensach}</td>
-                     <td>${list[i].soluong}</td>
-                     <td>${list[i].gia}</td>
-                     <td>          
-                            <label class="checkbox-container">
-                            <input type="checkbox">
-                            <span class="checkmark"></span>
-                            </label>
-                     </td>
-            </tr>
-        `;
-    }
+            <tr class="listsach">
+                <td>${item.Tensach}</td>
+                <td>${item.soluong}</td>
+                <td>${item.gia}</td>
+                <td>
+                    <label class="checkbox-container">
+                        <input type="checkbox">
+                        <span class="checkmark"></span>
+                    </label>
+                </td>
+            </tr>`
+            for(let j = 0; j < ctlist.length; j++){
+                if(ctlist[j].ten == list[i].Tensach){
+                `<tr class="detail">
+                    <td colspan="4">
+                        <div class="info">
+                            <img class="anh" src="${ctlist[j].anh}">
+                            <ul>
+                                <li><strong>Tác giả:</strong> ${ctlist[j].tacgia}</li>
+                                <li><strong>Ngày:</strong> ${ctlist[j].date}</li>
+                                <li><strong>Tóm tắt:</strong> ${ctlist[j].tomtat}</li>
+                            </ul>
+                        </div>
+                    </td>
+                </tr>`
+                }
+        }
+     }
 }
 
 render();
+
+let rows = document.querySelectorAll(".listsach");
+
+rows.forEach(row => {
+    row.addEventListener("click", () => {
+        let detail = row.parentElement.querySelector(".detail");
+
+        if (detail) {
+            detail.classList.toggle("active");
+        }
+    });
+});
